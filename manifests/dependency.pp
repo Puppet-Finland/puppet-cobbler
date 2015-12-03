c8bc12ff63370f10616dadc19480c917f04e862b
Samuli Seppänen <samuli.seppanen@gmail.com>
2015-12-03 15:01:11 +0200

    Add selinux configuration to dependency.pp
    Signed-off-by: Samuli Seppänen <samuli.seppanen@gmail.com>

--- manifests/dependency.pp
+++ manifests/dependency.pp
@@ -11,4 +11,20 @@ class cobbler::dependency {
   include ::apache::mod::proxy_http
   include ::apache::mod::setenvif
 
+  # The selinux settings need to be tweaked before we attempt to integrate 
+  # Cobbler with it.
+  if $::selinux {
+    include ::selinux
+
+    $cobbler_booleans = [ 'cobbler_can_network_connect',
+                          'httpd_can_network_connect_cobbler',
+                          'httpd_serve_cobbler_files', ]
+
+    selboolean { $cobbler_booleans:
+      value      => on,
+      persistent => true,
+      require    => Class['apache'],
+    }
+  }
+
 }
