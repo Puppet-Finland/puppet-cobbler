bb2ccf1617b94e47e457fbb4ede53e5fdf3a85d0
Samuli Seppänen <samuli.seppanen@gmail.com>
2016-09-05 15:33:49 +0300

    Allow publishing kickstart/preseed files on Cobbler webserver
    Signed-off-by: Samuli Seppänen <samuli.seppanen@gmail.com>

--- manifests/kickstart.pp
+++ manifests/kickstart.pp
@@ -1,7 +1,7 @@
 #
 # == Define: cobbler::kickstart
 #
-# Manage kickstart files in Cobbler
+# Manage kickstart/preseed files in Cobbler
 #
 # == Parameters
 #
@@ -10,22 +10,36 @@
 #   file must be present on the Puppet fileserver.
 # [*ensure*]
 #   The state of the kickstart file. Valid values are 'present' and 'absent'.
+# [*publish*]
+#   Place the kickstart/preseed file to its webserver 
+#   at http://<server>/cobbler/ks_mirror/<title>. Valid values are true and
+#   false (default).
 #
 define cobbler::kickstart
 (
-    $ensure = 'present'
+    $ensure = 'present',
+    Boolean $publish = false
 )
 {
     validate_string($title)
     validate_re($ensure, ['^present$', '^absent$'])
 
-    file { "cobbler-kickstart-${name}":
+    File {
         ensure  => $ensure,
-        name    => "/var/lib/cobbler/kickstarts/${title}",
         source  => "puppet:///files/${title}",
         owner   => root,
         group   => root,
         mode    => '0644',
         require => Class['cobbler::install'],
+    }
+
+    file { "cobbler-kickstart-${name}":
+        name => "/var/lib/cobbler/kickstarts/${title}",
+    }
+
+    if $publish {
+        file { "cobbler-kickstart-public-${name}":
+            name => "/var/www/cobbler/ks_mirror/${title}",
+        }
     }
 }
